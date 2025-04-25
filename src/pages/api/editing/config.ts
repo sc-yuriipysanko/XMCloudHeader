250124f3a51ac50cf070b4f41fa5ee5099d3dc82
Yurii Pysanko <96776380+sc-yuriipysanko@users.noreply.github.com>
2025-04-25 12:07:56 +0300

    Update config.ts

--- src/pages/api/editing/config.ts
+++ src/pages/api/editing/config.ts
@@ -1,6 +1,7 @@
 import { EditingConfigMiddleware } from '@sitecore-jss/sitecore-jss-nextjs/editing';
 import { components } from 'temp/componentBuilder';
 import metadata from 'temp/metadata.json';
+import { EditMode } from '@sitecore-jss/sitecore-jss-nextjs';
 
 /**
  * This Next.js API route is used by Sitecore editors (Pages) in XM Cloud
@@ -10,6 +11,7 @@ import metadata from 'temp/metadata.json';
 const handler = new EditingConfigMiddleware({
   components,
   metadata,
+  pagesEditMode: EditMode.Chromes,
 }).getHandler();
 
 export default handler;
